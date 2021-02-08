cf086a60473de04aafab11c2c9e08ca2519126af
Kirill Reuka <reuka.kirill@gmail.com>
2021-02-08 19:29:57 +0200

    Done Task1

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,21 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-//        Fill the array with data (random). Print out. Replace all negative values with 0. Print.
-        int[] arr = new int[10];
+        Task1.task1();
+//        Scanner scanner = new Scanner(System.in);
+//        Random random = new Random();
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = -10 + random.nextInt(10 + 10);
-        }
-        System.out.println(Arrays.toString(arr));
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 0) {
-                arr[i] = 0;
-            }
-        }
-        System.out.println(Arrays.toString(arr));
 
-//        The user enters the number of lines, then the values of these lines. The program calculates the average length of lines and displays on the screen.
+/*        The user enters the number of lines, then the values of these lines. The program calculates the average length of lines and displays on the screen.
         int n = scanner.nextInt();
         int len = 0;
         for (int i = 0; i < n; i++) {
             String s = scanner.next();
             len += s.length();
         }
-        System.out.println(len/n);
-//        The user enters the values of two variables. The program implements the basic mathematical operations on these variables (+ - * / %).
+        System.out.println(len/n);*/
+
+/*        The user enters the values of two variables. The program implements the basic mathematical operations on these variables (+ - * / %).
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
@@ -37,5 +27,6 @@ public class Main {
         System.out.println(a * b);
         System.out.println(a / b);
         System.out.println(a % b);
+ */
     }
 }
---
+++ src/Task1.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Arrays;
+
+
+public class Task1 {
+    public static void task1() {
+        //        Fill the array with data (random). Print out. Replace all negative values with 0. Print.
+        Random random = new Random();
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -10 + random.nextInt(10 + 10);
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                arr[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
